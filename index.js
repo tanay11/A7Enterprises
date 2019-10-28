73251b6d825a9ab716aa2c0493e1b10a2381717d
Tanay Mainkar <tanay.mainkar@lntinfotech.com>
2019-10-28 13:51:25 +0530

    Increament bug fix

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/api/form", (req, res) => {
 	const mailOptions = {
 		from: "a7.nasik@gmail.com", // sender address
 		to: req.body.email,
-		cc: "a7.nasik@gmail.com", // list of receivers
+		cc: "a7.nasik@gmail.com",
 		subject: "Registration successful", // Subject line
 		html: `<div><h2>Lucian Paints Welcomes You..</h2> <br/>Details -  ${JSON.stringify(req.body)}</div>`
 
